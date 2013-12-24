f1e9db4ece7a70f4b60353dd3c649c1e36649b9e
mushky <msazaidi@gmail.com>
2013-12-24 11:32:25 -0800

    added all the things

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Voterapp::Application.config.secret_key_base = 'aeda33063b29004e3343ed981af7f23dcecc4f4c8671d5088520ea0805af3a1a5475afcbb7982581e35eb1cda6238d379d9232d837d545dfc05fc3bb037000c6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Voterapp::Application.config.session_store :cookie_store, key: '_voterapp_session'
