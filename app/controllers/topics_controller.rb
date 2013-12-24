b98e78e12a6720de3a044d91f0535a96bbb140e1
mushky <msazaidi@gmail.com>
2013-12-24 14:42:53 -0800

    Add Vote Model, Controller, Views, and Routes

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,28 +1,32 @@
 class TopicsController < ApplicationController
   before_action :set_topic, only: [:show, :edit, :update, :destroy]
 
-  # GET /topics
-  # GET /topics.json
   def index
     @topics = Topic.all
   end
 
-  # GET /topics/1
-  # GET /topics/1.json
   def show
   end
 
-  # GET /topics/new
   def new
     @topic = Topic.new
   end
 
-  # GET /topics/1/edit
   def edit
   end
 
-  # POST /topics
-  # POST /topics.json
+  def upvote
+    @topic = Topic.find(params[:id])
+    @topic.votes.create #call votes method on Topic adding an upvote
+    redirect_to(topics_path)
+  end
+
+  def downvote
+    @topic = Topic.find(params[:id])
+    @topic.votes.destroy
+    redirect_to(topics_path)
+  end
+
   def create
     @topic = Topic.new(topic_params)
 
@@ -37,8 +41,6 @@ class TopicsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /topics/1
-  # PATCH/PUT /topics/1.json
   def update
     respond_to do |format|
       if @topic.update(topic_params)
@@ -51,8 +53,6 @@ class TopicsController < ApplicationController
     end
   end
 
-  # DELETE /topics/1
-  # DELETE /topics/1.json
   def destroy
     @topic.destroy
     respond_to do |format|
@@ -62,12 +62,10 @@ class TopicsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_topic
       @topic = Topic.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def topic_params
       params.require(:topic).permit(:title, :description)
     end
