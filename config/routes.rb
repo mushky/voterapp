e103c5f35063d27b6f10d9fb5af69b415d06fbc1
mushky <msazaidi@gmail.com>
2013-12-24 15:28:15 -0800

    update view index

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Voterapp::Application.routes.draw do
       post 'upvote'
     end
   end
-  
+
   root 'topics#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
